ecc8509858aed6c4a152f4d661aa3308b7dc6a58
Fabio Spampinato <spampinabio@gmail.com>
2023-08-12 12:51:42 +0100

    Log: updated styling of the warning logger

--- src/prompts/log.ts
+++ src/prompts/log.ts
@@ -8,8 +8,8 @@ import color from 'tiny-colors';
 const log = {
   success: ( message: string ): void => console.log ( `${color.green ( '✔' )} ${message}` ),
   error: ( message: string ): void => console.log ( `${color.red ( '✖' )} ${message}` ),
-  warning: ( message: string ): void => console.log ( `${color.yellow ( '⚠' )} ${message}` ),
   question: ( message: string ): void => console.log ( `${color.cyan ( color.bold ( '?' ) )} ${message}` ),
+  warning: ( message: string ): void => console.log ( `${color.yellow ( '⚠' )} ${color.yellow ( message )}` ),
   info: ( message: string ): void => console.log ( `${color.cyan ( 'ℹ' )} ${message}` )
 };
 
